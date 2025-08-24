82c7e3d7fae85f7d046e042194769b07449e6cc0
naveenaya <srinaveenaya99@gmail.com>
2025-08-24 17:51:33 +0530

    uoks

--- edit.php
+++ edit.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$root = _DIR_ . '/';
+$root = __DIR__. /';
 require_once $root . 'db.php';
 
 if (session_status() === PHP_SESSION_NONE) {
